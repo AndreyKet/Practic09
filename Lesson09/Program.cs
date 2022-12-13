fee87f4f643417a2b56f8c124a1cdeda4c2090a6
Andrey <man.liher66@gmail.com>
2022-12-13 23:11:25 +0300

    Lesson09

---
+++ Lesson09/Program.cs
@@ -0,0 +1,28 @@
+﻿// Console.Write("Введите число: ");
+// int value = int.Parse(Console.ReadLine());
+// Console.WriteLine(NaturNumber(value));
+
+// string NaturNumber( int value )
+// {
+//     if (value == 1) return "1";
+//     return NaturNumber(value - 1) + value;
+// }
+
+
+// Console.Write("Введите число A: ");
+// int valueA = int.Parse(Console.ReadLine());
+// Console.Write("Введите степень B: ");
+// int valueB = int.Parse(Console.ReadLine());
+// Console.WriteLine(Math(valueA, valueB));
+
+// int Math(int valueA, int valueB)
+// {
+//     // int result = 1;
+//     // for (int i = 0; i < valueB; i++)
+//     // {
+//     //     result *= valueA;
+//     // }
+//     // Console.WriteLine(result);
+//     if (valueB == 0) return 1;
+//     return Math(valueA, valueB - 1) * valueA;
+// }
